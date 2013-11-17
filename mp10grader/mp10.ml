165caf63337f0485a3e70e960bdede6dfbcaa451
Furquan Shaikh <furquan.m.shaikh@gmail.com>
2013-11-16 20:44:16 -0600

    Only handle remaining
    Signed-off-by: Furquan Shaikh <furquan.m.shaikh@gmail.com>

--- mp10grader/mp10.ml
+++ mp10grader/mp10.ml
@@ -13,10 +13,10 @@ let monOpApply unop v =
   match (unop,v) with
       (IntNegOp, IntVal i) -> IntVal(-i)
     | (HdOp, ListVal lst) -> 
-      (match lst with [] -> failwith "list is empty"
+      (match lst with [] -> Exn(0)
 	| x::xs -> x)
     | (TlOp, ListVal lst) ->
-      (match lst with [] -> failwith "list is empty"
+      (match lst with [] -> Exn(0)
 	| x::xs -> ListVal(xs) )
     | (FstOp, PairVal(v1,_)) -> v1
     | (SndOp, PairVal(_,v2)) -> v2
@@ -28,11 +28,11 @@ let binOpApply binop v1 v2 =
       (IntPlusOp, IntVal i1, IntVal i2) -> IntVal (i1+i2)
     | (IntMinusOp, IntVal i1, IntVal i2) -> IntVal (i1-i2)
     | (IntTimesOp, IntVal i1, IntVal i2) -> IntVal (i1*i2)
-    | (IntDivOp, IntVal i1, IntVal i2) -> IntVal (i1/i2)
+    | (IntDivOp, IntVal i1, IntVal i2) -> if i2 = 0 then Exn(0) else IntVal (i1/i2)
     | (RealPlusOp, RealVal r1, RealVal r2) -> RealVal (r1+.r2)
     | (RealMinusOp, RealVal r1, RealVal r2) -> RealVal (r1-.r2)
     | (RealTimesOp, RealVal r1, RealVal r2) -> RealVal (r1*.r2)
-    | (RealDivOp, RealVal r1, RealVal r2) -> RealVal (r1/.r2)
+    | (RealDivOp, RealVal r1, RealVal r2) -> if r2 = 0.0 then Exn(0) else RealVal (r1/.r2)
     | (ConcatOp, StringVal s1, StringVal s2) -> StringVal (s1^s2)
     | (ConsOp, h, ListVal t) -> ListVal (h::t)
     | (CommaOp, v1, v2) -> PairVal(v1,v2)
@@ -44,31 +44,63 @@ let rec eval_exp (exp, m) =
   match exp with
       ConstExp c -> const_to_val c
     | VarExp x -> (match lookup_env m x with None -> failwith "couldnt find the variable."
+                                       	   | Some RecVarVal(x',e, m') -> ClosureVal (x',e,ins_env m x' (RecVarVal(x',e, m')))
 	                                   | Some v -> v)
     | FnExp(x,e) -> ClosureVal(x,e,m)
     | AppExp(e1,e2) ->
       (match eval_exp (e1,m) with
-	  ClosureVal(x,e',m') -> 
+	  Exn(i) -> Exn(i)
+	| ClosureVal(x,e',m') -> 
 	    (match eval_exp (e2,m) with
-		v' -> eval_exp (e',(ins_env m' x v')))
+		Exn(i) -> Exn(i)
+	      |	v' -> eval_exp (e',(ins_env m' x v')))
 	| _ -> failwith "invalid return of closure")
-    | MonOpAppExp(mon,e) -> let v = eval_exp (e,m) in monOpApply mon v
+    | MonOpAppExp(mon,e) ->
+      (match eval_exp (e,m) with
+	  Exn(i) -> Exn(i)
+	| v -> monOpApply mon v
+      )
     | BinOpAppExp(b,e1,e2) -> 
-      let v1 = eval_exp (e1,m) in
-      let v2 = eval_exp (e2,m) in
-      binOpApply b v1 v2
+      (match eval_exp (e1,m) with
+	  Exn(i) -> Exn(i)
+	| v1 -> (match eval_exp (e2,m) with
+	    Exn(j) -> Exn(j)
+	    | v2 -> binOpApply b v1 v2
+	    ))
     | IfExp(e1,e2,e3) ->
       (match eval_exp (e1,m) with
 	  BoolVal(true) -> eval_exp (e2,m)
 	| BoolVal(false) -> eval_exp (e3,m)
+	| Exn(i) -> Exn(i)
 	| _ -> failwith "invalid return value for if")
     | LetExp(d,e) ->
-      let (b,m') = eval_dec (d,m) in
-      eval_exp (e,(sum_env m' m))
-    | _ -> failwith "eval_exp is not implemented yet."
+      (match eval_dec (d,m) with
+	  ((None,Exn(i))::tl,m') -> Exn(i)
+	| (b,m') -> eval_exp (e,(sum_env m' m)))
+    | RaiseExp(e) ->
+      (match eval_exp (e,m) with
+	  Exn(i) -> Exn(i)
+	| IntVal(v) -> Exn(v)
+	| _ -> failwith "Invalid argument to raise")
+    | HandleExp(e,int_opt,e0,lst) ->
+      (match eval_exp (e,m) with
+	  v -> v
 
 and eval_dec (dec, m) = 
   match dec with
-      Val(x,e) -> let v = eval_exp (e,m) in 
-		  if x = "" then ([(None,v)],empty_env) else ([(Some x,v)],(make_env x v))
-    | _ -> failwith "eval_dec is not implemented yet."
+      Val(x,e) -> 
+	(match eval_exp (e,m) with
+	    Exn(i) -> ([(None,Exn(i))],empty_env)
+	  | v -> if x = "" then ([(None,v)],empty_env) else ([(Some x,v)],(make_env x v)))
+    | Seq(d1,d2) -> 
+      (match eval_dec (d1,m) with
+	  ((None,Exn(i))::tl,m') -> ((None,Exn(i))::tl,m')
+	| (b1,m') ->
+	  (match eval_dec (d2, (sum_env m' m)) with
+	      ((None,Exn(i))::tl,m') -> ((None,Exn(i))::tl,m')
+	    | (b2,m'') -> (b2@b1, sum_env m'' m')))
+    | Local(d1,d2) -> 
+      (match eval_dec (d1,m) with
+	  ((None,Exn(i))::tl,m') -> ((None,Exn(i))::tl,empty_env)
+	| (b1,m') -> eval_dec (d2, (sum_env m' m)))
+    | Rec(f,x,e) -> ([(Some f, RecVarVal(x,e,m))], (make_env f (RecVarVal(x,e,m))))
